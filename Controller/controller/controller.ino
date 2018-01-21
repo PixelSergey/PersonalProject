787031bc0079573f558197b6eea3d108e8171078
PixelSergey <sergey.ichtchenko@gmail.com>
2018-01-21 20:51:24 +0200

    Create joystick-arduino interfacing
    I will now post progress pictures in the '/Controller/photos' directory

--- Controller/controller/controller.ino
+++ Controller/controller/controller.ino
@@ -1,9 +1,29 @@
-void setup() {
-  
+const int joyX = A0;
+const int joyY = A1; 
+const int joySw = 12;
 
+int x = 0;
+int y = 0;
+int sw = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(joyX, INPUT);
+  pinMode(joyY, INPUT);
+  pinMode(joySw, INPUT);
 }
 
 void loop() {
-  
+  x = map(analogRead(joyX), 0, 1023, 0, 255);
+  y = map(analogRead(joyY), 0, 1023, 0, 255);
+  sw = digitalRead(joySw);
 
+  Serial.print("X value: ");
+  Serial.print(x);
+  Serial.print("\t Y value: ");
+  Serial.print(y);
+  Serial.print("\t Pressed? ");
+  Serial.println(1-sw);
+  
+  delay(2);
 }
